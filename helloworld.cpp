98ed0aa79fc0696740258dc961f872969cfe47bc
88888812 <@yandex.ru>
2021-03-17 11:42:54 +0300

    mozilla

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <string>
+<<<<<<< HEAD
 // check pull request problem 
 int main()
+=======
+
+int
+main()
+>>>>>>> mozilla
 {
-std::string name;
-std::cin » name;
-std::cout « "Hello World!"«name;
+  std::string name;
+  std::cin » name;
+  std::cout « "Hello World!"«name;
 }
